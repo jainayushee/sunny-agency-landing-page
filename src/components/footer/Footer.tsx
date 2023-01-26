15f79263312f183e710b6db23a5241c2c3dc8ddb
jainayushee <jainayushee5@gmail.com>
2023-01-26 18:57:02 +0530

    Update footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,81 +1,30 @@
 import React from 'react'
 
-export default function footer() {
+export default function footer () {
+  const links: string[] = ['About', 'Services', 'Projects']
+  const icons: string[] = ['facebook-square', 'twitter', 'pinterest', 'instagram']
+
   return (
-    <footer className="bg-[#312e81]">
-      
-      <div
-        className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0"
-      >
-       
-        <div
-          className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start"
-        >
-          <div className="mx-auto my-6 text-center text-white md:hidden">
-            Copyright &copy; 2022, All Rights Reserved
-          </div>
-         
-          <div>
-            <img src="img/logo-white.svg" className="h-8" alt="" />
-          </div>
-
-          <div className="flex justify-center space-x-4">
-           
-            <a href="#">
-              <img src="img/icon-facebook.svg" alt="" className="h-8" />
-            </a>
-           
-            <a href="#">
-              <img src="img/icon-youtube.svg" alt="" className="h-8" />
-            </a>
-           
-            <a href="#">
-              <img src="img/icon-twitter.svg" alt="" className="h-8" />
-            </a>
-           
-            <a href="#">
-              <img src="img/icon-pinterest.svg" alt="" className="h-8" />
-            </a>
-         
-            <a href="#">
-              <img src="img/icon-instagram.svg" alt="" className="h-8" />
-            </a>
+    <footer className='py-10 bg-[#afe4da]'>
+      <div className='row justify-content-center'>
+        <div className='py-3 text-center'>
+          <h2 className='text-3xl text-[#25564b]'>sunnyside</h2>
+        </div>
+        <div id='footer-links' className='flex flex-row justify-center my-2 py-3'>
+          <div className='text-[#458c7e]'>
+            {links.map((link: string) => (
+              <a className='mx-2' href='#'>{link}</a>
+            ))}
           </div>
         </div>
-      
-        <div className="flex justify-around space-x-32">
-          <div className="flex flex-col space-y-3 text-white">
-            <a href="#" className="hover:text-brightRed">Home</a>
-            <a href="#" className="hover:text-brightRed">Pricing</a>
-            <a href="#" className="hover:text-brightRed">Products</a>
-            <a href="#" className="hover:text-brightRed">About</a>
-          </div>
-          <div className="flex flex-col space-y-3 text-white">
-            <a href="#" className="hover:text-brightRed">Careers</a>
-            <a href="#" className="hover:text-brightRed">Community</a>
-            <a href="#" className="hover:text-brightRed">Privacy Policy</a>
-          </div>
-        </div>
-
-        
-        <div className="flex flex-col justify-between">
-          <form>
-            <div className="flex space-x-3">
-              <input
-                type="text"
-                className="flex-1 px-4 rounded-full focus:outline-none"
-                placeholder="Updated in your inbox"
-              />
-              <button
-                className="px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedLight focus:outline-none"
-              >
-                Go
-              </button>
-            </div>
-          </form>
-          <div className="hidden text-white md:block">
-            Copyright &copy; 2022, All Rights Reserved
-          </div>
+        <div id='footer-icons' className='flex flex-row justify-center py-3'>
+          {icons.map((iconName: string) => {
+            return (
+              <a className='nav-link text-[#458c7e] mx-3' href='#'>
+                <i className={`fab fa-${iconName} fa-lg`} />
+              </a>
+            )
+          })}
         </div>
       </div>
     </footer>

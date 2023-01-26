23590679fc55c984973b421d1748777c8081034b
jainayushee <jainayushee5@gmail.com>
2023-01-26 22:57:01 +0530

    Fix grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -5,16 +5,16 @@ export default function RowOne () {
     <>
       <div className='flex flex-row'>
         <div className='basis-6/12'>
-          <div className='h-full flex flex-col items-center justify-center p-8'>
-            <h1 className='text-[#212529]'>Transform your brand</h1>
+          <div className='h-full flex flex-col items-start justify-center px-20'>
+            <h1 className='text-[#23303e] leading-8 pb-5'
+              style={{ fontWeight: '900', fontSize: '2rem' }}>Transform your brand</h1>
 
-            <p className=''>
-              We are a full-service creative agency specializing in helping
-              brands grow fast. Engage your clients through compelling visuals
-              that do most of the marketing for you.
+            <p style={{ fontFamily: '"Barlow", sans-serif' }} className='pb-5'>
+              We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.
             </p>
 
-            <a href='#' className='' >Learn More</a >
+            <a href='#' className='uppercase' >Learn More</a >
+            <div className='cursor-pointer h-2 w-28 bg-[#fad400] rounded-md opacity-40 hover:opacity-80' />
           </div>
         </div>
         <div className='basis-6/12'>
@@ -26,15 +26,15 @@ export default function RowOne () {
           <img src='img/image-stand-out.jpg' />
         </div>
         <div className='basis-6/12'>
-          <div className='h-full flex flex-col items-center justify-center p-8'>
-            <h1 className='text-[#212529]'>Stand out to the right audience</h1>
+          <div className='h-full flex flex-col items-start justify-center px-20'>
+            <h1 className='text-[#23303e] leading-8 pb-5' style={{ fontWeight: '900', fontSize: '2rem' }}>Stand out to the right audience</h1>
 
-            <p className=''>
+            <p style={{ fontFamily: '"Barlow", sans-serif' }} className='pb-5'>
               Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.
-
             </p>
 
-            <a href='#' className='' >Learn More</a >
+            <a href='#' className='uppercase' >Learn More</a >
+            <div className='cursor-pointer h-2 w-28 bg-[#fe7867] rounded-md opacity-40 hover:opacity-80' />
           </div>
         </div>
 
